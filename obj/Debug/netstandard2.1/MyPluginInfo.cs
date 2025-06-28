f5ca9fb6080c1eb043294f04ff10fb1dd2ca735e
Jason Papp <jspapp@gmail.com>
2025-06-28 17:25:00 -0400

    Remove build files

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ProgressMap
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "ProgressMap";
-        public const string PLUGIN_NAME = "Altimeter";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
