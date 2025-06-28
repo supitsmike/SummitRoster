8b2bceacb736aeb50c7c5371b0344c79925abfe1
Jason Papp <jspapp@gmail.com>
2025-06-28 17:21:11 -0400

    Init commit

---
+++ obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace ProgressMap
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "ProgressMap";
+        public const string PLUGIN_NAME = "Altimeter";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
